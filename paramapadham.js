9a0fd7a159d929ce55ff9ac942ea5caf4c6b46d9
kotteshtechboss07 <kottesh2005@gmail.com>
2024-03-11 15:44:58 +0530

    javascript_games

---
+++ paramapadham.js
@@ -0,0 +1,5 @@
+"use strict"
+const startPage=document.querySelector('.main--0');
+const gamepage=document.querySelector('.main--1');
+const coinP0=document.querySelector('.#P0--coin');
+const coinP1=document.querySelector('.#P1--coin');
